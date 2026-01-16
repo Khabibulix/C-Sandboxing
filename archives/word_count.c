2c465001be6168292f6251f895674bcac39678c4
Khabibulix <unitetempete@gmail.com>
2026-01-16 16:41:53 +0100

    word_count: basic word_count finished

--- archives/word_count.c
+++ archives/word_count.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX_WORD_LEN 50
+#define MAX_WORDS 1000
+
+typedef struct {
+    char word[MAX_WORD_LEN];
+    int count;
+} WordEntry;
+
+WordEntry words[MAX_WORDS];
+int word_count = 0;
 
 
 FILE* open_file(const char* filename){
@@ -19,6 +29,23 @@ int is_valid_char(int c){
             (c >= '0' && c <= '9'));
 }
 
+
+void update_word_count(const char* word){
+    for (int i = 0; i < word_count; i++){
+        if (strcmp(words[i].word, word) == 0){
+            words[i].count++;
+            return;
+        }
+    }
+    if (word_count < MAX_WORDS){
+        strcpy(words[word_count].word, word);
+        words[word_count].count = 1;
+        word_count++;
+    } else {
+        fprintf(stderr, "Max words is overflowed\n");
+    }
+}
+
 void parse_words(FILE* f){
     char buffer[MAX_WORD_LEN];
     int index = 0;
@@ -32,7 +59,7 @@ void parse_words(FILE* f){
         } else {
             if (in_word){
                 buffer[index] = '\0';
-                printf("Word detected : %s\n", buffer);
+                update_word_count(buffer);
                 index = 0;
                 in_word = 0;
             }
@@ -42,7 +69,13 @@ void parse_words(FILE* f){
     // last word
     if (in_word){
         buffer[index] = '\0';
-        printf("Word detected : %s\n", buffer);
+        update_word_count(buffer);
+    }
+}
+
+void print_word_counts(){
+    for (int i = 0; i < word_count; i++){
+        printf("%s : %d\n", words[i].word, words[i].count);
     }
 }
 
@@ -52,6 +85,7 @@ int main (void){
     if (f == NULL) return 1;
 
     parse_words(f);
+    print_word_counts();
 
     fclose(f);
 }
