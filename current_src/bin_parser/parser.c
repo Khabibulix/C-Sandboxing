e452cbb2a6ef022f503de0af01d83a6763ad54d3
Khabibulix <unitetempete@gmail.com>
2026-01-28 02:06:22 +0100

    binary_dump: detecting ASCII values in bytes

--- current_src/bin_parser/parser.c
+++ current_src/bin_parser/parser.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+
+struct annotations {
+    size_t offset;
+    uint8_t value;
+    char comment[64];
+};
 
 int main(){
     FILE *f = fopen("dummy.bin", "rb");
@@ -9,7 +16,7 @@ int main(){
     fseek(f, 0, SEEK_END);
     long size = ftell(f);
     rewind(f);
-
+    
     uint8_t *buf = malloc(size);
     if (!buf) return 2;
 
@@ -17,10 +24,33 @@ int main(){
     fclose(f);
 
     size_t cursor = 0;
+    size_t start_offset = 0;
+    char ascii_seq_temp[64] = "";
+    size_t ascii_index = 0;
 
-    while (cursor < 16){
-         uint8_t oct = buf[cursor];
-        printf("offset %zu : value 0x%02X\n", cursor, oct);
+    //TODO: Replace by dynamic struct for bigger files
+    struct annotations annotation[size];
+
+    while (cursor < size){
+        uint8_t oct = buf[cursor];
+
+        annotation[cursor].offset = cursor;
+        annotation[cursor].value = oct;
+        annotation[cursor].comment[0] = '\0';
+
+        if (oct >= 0x20 && oct <= 0x7E){
+            strcpy(annotation[cursor].comment, "ASCII printable");
+            if (ascii_index == 0) start_offset = cursor;
+            ascii_seq_temp[ascii_index++] = oct;
+            ascii_seq_temp[ascii_index] = '\0';
+
+        } else {
+            if (ascii_index > 0){
+                printf("Offset %zu-%zu : ASCII = %s\n", start_offset, cursor-1, ascii_seq_temp );
+                ascii_index = 0;
+                ascii_seq_temp[0] = '\0';
+            }
+        }
         cursor++;
     }
 
