e4e203ffc52b843b9e421ba3e563831761a4679d
Khabibulix <unitetempete@gmail.com>
2026-01-28 02:22:35 +0100

    binary_dump: realloc for dynamic struct

--- current_src/bin_parser/parser.c
+++ current_src/bin_parser/parser.c
@@ -9,6 +9,7 @@ struct annotations {
     char comment[64];
 };
 
+
 int main(){
     FILE *f = fopen("dummy.bin", "rb");
     if (!f) return 1;
@@ -27,19 +28,29 @@ int main(){
     size_t start_offset = 0;
     char ascii_seq_temp[64] = "";
     size_t ascii_index = 0;
+    size_t current_allocated_size = size;
 
-    //TODO: Replace by dynamic struct for bigger files
-    struct annotations annotation[size];
+    struct annotations *annotation_ptr = malloc(size * sizeof(struct annotations));
+    if (!annotation_ptr) return 1;
+
+
 
     while (cursor < size){
         uint8_t oct = buf[cursor];
 
-        annotation[cursor].offset = cursor;
-        annotation[cursor].value = oct;
-        annotation[cursor].comment[0] = '\0';
+        if (cursor >= current_allocated_size){
+            size_t new_size = current_allocated_size * 2;
+            annotation_ptr = realloc(annotation_ptr, new_size * sizeof(struct annotations));
+            if (!annotation_ptr) exit(1);
+            current_allocated_size = new_size;
+        }
+
+        annotation_ptr[cursor].offset = cursor;
+        annotation_ptr[cursor].value = oct;
+        annotation_ptr[cursor].comment[0] = '\0';
 
         if (oct >= 0x20 && oct <= 0x7E){
-            strcpy(annotation[cursor].comment, "ASCII printable");
+            strcpy(annotation_ptr[cursor].comment, "ASCII printable");
             if (ascii_index == 0) start_offset = cursor;
             ascii_seq_temp[ascii_index++] = oct;
             ascii_seq_temp[ascii_index] = '\0';
@@ -55,5 +66,6 @@ int main(){
     }
 
     free(buf);
+    free(annotation_ptr);
     return 0;
 }
